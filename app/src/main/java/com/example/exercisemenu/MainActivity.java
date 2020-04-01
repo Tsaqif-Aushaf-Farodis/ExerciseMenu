c50f606ad7f2f1d4e41196242395f559e5daec40
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-04-01 18:49:22 +0700

    Deklarasi variabel yg ada di activity_main

--- app/src/main/java/com/example/exercisemenu/MainActivity.java
+++ app/src/main/java/com/example/exercisemenu/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.exercisemenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText addUsername, addPassword;
+    Button btnLogin;
+    String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
