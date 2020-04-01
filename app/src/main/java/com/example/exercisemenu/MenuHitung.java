78c94517bd99e25b96df8fea554369cc2ded899a
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-04-01 21:53:47 +0700

    find view by id from activity_menu_hitung

--- app/src/main/java/com/example/exercisemenu/MenuHitung.java
+++ app/src/main/java/com/example/exercisemenu/MenuHitung.java
@@ -7,10 +7,17 @@ import android.widget.TextView;
 
 public class MenuHitung extends AppCompatActivity {
     TextView tv_persegi, tv_lingkaran, tv_persegiPanjang, tv_segitiga, tv_trapesium;
+    String persegi, lingkaran, persegiPanjang, segitiga, trapesium;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_hitung);
+
+        tv_persegi = (TextView) findViewById(R.id.tvPersegi);
+        tv_lingkaran = (TextView) findViewById(R.id.tvLingkaran);
+        tv_persegiPanjang = (TextView) findViewById(R.id.tvPersegiPanjang);
+        tv_segitiga = (TextView) findViewById(R.id.tvSegitiga);
+        tv_trapesium = (TextView) findViewById(R.id.tvTrapesium);
     }
 }
