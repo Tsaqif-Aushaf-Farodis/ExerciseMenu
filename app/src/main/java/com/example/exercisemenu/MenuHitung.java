979e58b05e39d82e961073450baad1d7503ef756
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-04-01 19:51:26 +0700

    Membuat activity & UI Menu Hitung Bangun Datar

---
+++ app/src/main/java/com/example/exercisemenu/MenuHitung.java
@@ -0,0 +1,14 @@
+package com.example.exercisemenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuHitung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_hitung);
+    }
+}
