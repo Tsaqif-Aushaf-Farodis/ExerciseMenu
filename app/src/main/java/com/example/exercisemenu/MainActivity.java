aad79a51ea6f09be4257f1516136695158d3b927
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-04-01 19:55:00 +0700

    Membuat function btnLogin untuk pindah ke activity_menu_hitung

--- app/src/main/java/com/example/exercisemenu/MainActivity.java
+++ app/src/main/java/com/example/exercisemenu/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.exercisemenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,9 +30,10 @@ public class MainActivity extends AppCompatActivity {
                 username   = addUsername.getText().toString();
                 password = addPassword.getText().toString();
 
-                //Menampilkan PopUp
                 if (username.equals("admin")||password.equals("123")){
-
+                    //Pindah activity
+                    Intent i = new Intent(getApplicationContext(),MenuHitung.class);
+                    startActivity(i);
                 }else{
                     Toast.makeText(getApplicationContext(),
                             "Login Failed", Toast.LENGTH_LONG).show();
