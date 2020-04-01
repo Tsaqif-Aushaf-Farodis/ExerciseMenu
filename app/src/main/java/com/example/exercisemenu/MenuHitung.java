d6b2e5dd81476e04a7e4c576f94432e9ca5047cd
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-04-01 22:01:14 +0700

    add onCLick for all variable textView

--- app/src/main/java/com/example/exercisemenu/MenuHitung.java
+++ app/src/main/java/com/example/exercisemenu/MenuHitung.java
@@ -3,6 +3,7 @@ package com.example.exercisemenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MenuHitung extends AppCompatActivity {
@@ -19,5 +20,40 @@ public class MenuHitung extends AppCompatActivity {
         tv_persegiPanjang = (TextView) findViewById(R.id.tvPersegiPanjang);
         tv_segitiga = (TextView) findViewById(R.id.tvSegitiga);
         tv_trapesium = (TextView) findViewById(R.id.tvTrapesium);
+
+        tv_persegi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        tv_lingkaran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        tv_persegiPanjang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        tv_segitiga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        tv_trapesium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
