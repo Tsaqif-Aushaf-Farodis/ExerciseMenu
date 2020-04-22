c44c98d7fb8cd3a205ea6b4a343b9fc078d33d7b
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-04-22 10:41:59 +0700

    Update popup menu

--- app/src/main/java/com/example/exercisemenu/MenuHitung.java
+++ app/src/main/java/com/example/exercisemenu/MenuHitung.java
@@ -2,6 +2,7 @@ package com.example.exercisemenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 public class MenuHitung extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     TextView tv_persegi, tv_lingkaran, tv_persegiPanjang, tv_segitiga, tv_trapesium;
     String persegi, lingkaran, persegiPanjang, segitiga, trapesium;
+    int kode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +25,74 @@ public class MenuHitung extends AppCompatActivity implements PopupMenu.OnMenuIte
         tv_persegiPanjang = (TextView) findViewById(R.id.tvPersegiPanjang);
         tv_segitiga = (TextView) findViewById(R.id.tvSegitiga);
         tv_trapesium = (TextView) findViewById(R.id.tvTrapesium);
-    }
 
-    public void showMenu(View v){
-        PopupMenu menu = new PopupMenu(this, v);
-        menu.setOnMenuItemClickListener(this);
-        menu.inflate(R.menu.main_menu);
-        menu.show();
+        tv_persegi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kode=1;
+                PopupMenu popupMenu = new PopupMenu(getApplicationContext(), v);
+                popupMenu.setOnMenuItemClickListener(MenuHitung.this);
+                popupMenu.inflate(R.menu.main_menu);
+                popupMenu.show();
+            }
+        });
+
+        tv_lingkaran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kode=2;
+                PopupMenu popupMenu = new PopupMenu(getApplicationContext(), v);
+                popupMenu.setOnMenuItemClickListener(MenuHitung.this);
+                popupMenu.inflate(R.menu.main_menu);
+                popupMenu.show();
+            }
+        });
+
+        tv_persegiPanjang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kode=3;
+                PopupMenu popupMenu = new PopupMenu(getApplicationContext(), v);
+                popupMenu.setOnMenuItemClickListener(MenuHitung.this);
+                popupMenu.inflate(R.menu.main_menu);
+                popupMenu.show();
+            }
+        });
+
+        tv_segitiga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kode=4;
+                PopupMenu popupMenu = new PopupMenu(getApplicationContext(), v);
+                popupMenu.setOnMenuItemClickListener(MenuHitung.this);
+                popupMenu.inflate(R.menu.main_menu);
+                popupMenu.show();
+            }
+        });
+
+        tv_trapesium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kode=5;
+                PopupMenu popupMenu = new PopupMenu(getApplicationContext(), v);
+                popupMenu.setOnMenuItemClickListener(MenuHitung.this);
+                popupMenu.inflate(R.menu.main_menu);
+                popupMenu.show();
+            }
+        });
     }
 
     @Override
     public boolean onMenuItemClick(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.luas:
-                Toast.makeText(this, "Test Luas", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.keliling:
-                Toast.makeText(this, "Test Keliling", Toast.LENGTH_SHORT).show();
-                return true;
+        switch (item.getItemId()) {
+            case R.id.tvPersegi:
+                if (item.getItemId() == R.id.luas){
+                    Intent luasPersegi = new Intent(getApplicationContext(), LuasPersegi.class);
+                    startActivity(luasPersegi);
+                }else{
+                    Intent kelilingPersegi = new Intent(getApplicationContext(), KelilingPersegi.class);
+                    startActivity(kelilingPersegi);
+                }
             default:
                 return false;
         }
