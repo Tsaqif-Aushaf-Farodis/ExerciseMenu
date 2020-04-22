9c199f06e884acef391d1abfc476db5271e31963
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-04-22 13:40:19 +0700

    Buat function rumus keliling persegi

--- app/src/main/java/com/example/exercisemenu/KelilingPersegi.java
+++ app/src/main/java/com/example/exercisemenu/KelilingPersegi.java
@@ -3,12 +3,39 @@ package com.example.exercisemenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class KelilingPersegi extends AppCompatActivity {
+    EditText et_sisi;
+    Button btn_KelilingPersegi;
+    TextView tv_HasilKelilingPersegi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keliling_persegi);
+
+        et_sisi = findViewById(R.id.addSisiKeliling);
+        btn_KelilingPersegi = findViewById(R.id.btnKelilingPersegi);
+        tv_HasilKelilingPersegi = findViewById(R.id.hasilKelilingPersegi);
+
+        btn_KelilingPersegi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btn_KelilingPersegi.getText().toString().trim().isEmpty() ||
+                        et_sisi.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(KelilingPersegi.this,
+                            "Data Tidak Boleh Kosong", Toast.LENGTH_LONG).show();
+                }else {
+                    double sisi = Double.parseDouble(et_sisi.getText().toString());
+                    double keliling = 4 * sisi;
+                    tv_HasilKelilingPersegi.setText("Hasil Keliling Persegi Dengan Sisi: "+sisi+" adalah: "+keliling+"");
+                }
+            }
+        });
     }
 }
