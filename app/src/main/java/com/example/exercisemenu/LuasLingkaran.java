d8f844c801d977d139e33b9227f9fc8893dbf90e
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-04-24 12:43:47 +0700

    Buat function rumus Luas persegi lingkaran

--- app/src/main/java/com/example/exercisemenu/LuasLingkaran.java
+++ app/src/main/java/com/example/exercisemenu/LuasLingkaran.java
@@ -3,12 +3,40 @@ package com.example.exercisemenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LuasLingkaran extends AppCompatActivity {
+    EditText et_JariJari;
+    Button btn_LuasLingkaran;
+    TextView tv_HasilLuasLingkaran;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luas_lingkaran);
+
+        et_JariJari = findViewById(R.id.addJari2Luas);
+        btn_LuasLingkaran = findViewById(R.id.btnLuasLingkaran);
+        tv_HasilLuasLingkaran = findViewById(R.id.hasilLuasLingkaran);
+
+        btn_LuasLingkaran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(et_JariJari.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(LuasLingkaran.this,
+                            "Data Tidak Boleh Kosong", Toast.LENGTH_LONG).show();
+                }else {
+                    double jarijari = Double.parseDouble(et_JariJari.getText().toString());
+                    double Luas = 3.14 * jarijari * jarijari;
+                    //double Luas2 = 22/7 * jarijari * jarijari;
+                    //double Luas3 = Math.PI * jarijari * jarijari;
+                    tv_HasilLuasLingkaran.setText("Luas lingkaran dengan jari-jari "+jarijari+" adalah: "+Luas+"");
+                }
+            }
+        });
     }
 }
